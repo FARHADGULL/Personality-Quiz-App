acebc2a9f77dc37a3c902c98573aba8fedf1aa76
Farhad Khan Haadi <89342676+FARHADGULL@users.noreply.github.com>
2023-07-27 13:18:49 +0500

    now using stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,25 @@ void main() {
   runApp( MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  State<StatefulWidget> createState() => MyAppState();
+}
+
+class MyAppState extends State<MyApp> {
+  var questionIndex = 0; //var is a generic type and dart can infer the type of the variable
   void answerQuestion() {
-    print('Answer chosen!');
+    setState(() {
+      questionIndex = questionIndex + 1;
+    });
+    print(questionIndex);
   }
 
   @override
   Widget build(BuildContext context){
+    var questions = [
+      'What\'s your favourite color?',
+      'What\'s your favourite animal?',
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -18,9 +30,9 @@ class MyApp extends StatelessWidget {
         ),
         body: Column(
           children: <Widget>[
-            Text('The Questions!'),
-            ElevatedButton(onPressed: answerQuestion, child: Text('Answer 1'),),
-            ElevatedButton(onPressed: answerQuestion, child: Text('Answer 2'),),
+            Text(questions[questionIndex]), //Text widget 
+            ElevatedButton(onPressed: answerQuestion, child: Text('Answer 1'),), //called without parantheses because we don't want to execute it right away when flutter builds and render the widgets but rather when the button is pressed and called function using pointer and its a named function
+            ElevatedButton(onPressed: () => print('Answer 2 Chosen!'), child: Text('Answer 2'),), //anonymous function using lambda expression and called using pointer
             ElevatedButton(onPressed: answerQuestion, child: Text('Answer 3'),),
             ],
         ),
