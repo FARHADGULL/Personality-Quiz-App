92eb00e55a2333062726bed2ac4377f7197eb0b9
Farhad Khan Haadi <89342676+FARHADGULL@users.noreply.github.com>
2023-08-07 14:50:56 +0500

    updated the class from public to private

--- lib/main.dart
+++ lib/main.dart
@@ -5,15 +5,15 @@ void main() {
 }
 
 class MyApp extends StatefulWidget {
-  State<StatefulWidget> createState() => MyAppState();
+  State<StatefulWidget> createState() => _MyAppState();
 }
-class MyAppState extends State<MyApp> {
-  var questionIndex = 0; //var is a generic type and dart can infer the type of the variable
-  void answerQuestion() {
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0; //var is a generic type and dart can infer the type of the variable
+  void _answerQuestion() {
     setState(() {
-      questionIndex = questionIndex + 1;
+      _questionIndex = _questionIndex + 1;
     });
-    print(questionIndex);
+    print(_questionIndex);
   }
 
   @override
@@ -29,10 +29,10 @@ class MyAppState extends State<MyApp> {
         ),
         body: Column(
           children: <Widget>[
-            Text(questions[questionIndex]), //Text widget 
-            ElevatedButton(onPressed: answerQuestion, child: Text('Answer 1'),), //called without parantheses because we don't want to execute it right away when flutter builds and render the widgets but rather when the button is pressed and called function using pointer and its a named function
+            Text(questions[_questionIndex]), //Text widget 
+            ElevatedButton(onPressed: _answerQuestion, child: Text('Answer 1'),), //called without parantheses because we don't want to execute it right away when flutter builds and render the widgets but rather when the button is pressed and called function using pointer and its a named function
             ElevatedButton(onPressed: () => print('Answer 2 Chosen!'), child: Text('Answer 2'),), //anonymous function using lambda expression and called using pointer
-            ElevatedButton(onPressed: answerQuestion, child: Text('Answer 3'),),
+            ElevatedButton(onPressed: _answerQuestion, child: Text('Answer 3'),),
             ],
         ),
       ),
