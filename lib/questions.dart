15019f3cc627db7ae5ceedf23446e85b733660f7
Farhad Khan Haadi <89342676+FARHADGULL@users.noreply.github.com>
2023-08-08 11:59:15 +0500

    added few styling stuff

--- lib/questions.dart
+++ lib/questions.dart
@@ -3,10 +3,18 @@ import 'package:flutter/material.dart';
 class Question extends StatelessWidget {
   final String questionText;
 
-  Question(this.questionText);
+  const Question(this.questionText, {super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Text(questionText);
+    return Container(
+      width: double.infinity,
+      margin: const EdgeInsets.all(10), //EdgeInsets.all(10) is a constructor
+      child: Text(
+        questionText,
+        style: const TextStyle(fontSize: 28),
+        textAlign: TextAlign.center,
+      ),
+    );
   }
 }
