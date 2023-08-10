5cb843c6cfab9d027fe1140be0521a3d3dbe169a
Farhad Khan Haadi <89342676+FARHADGULL@users.noreply.github.com>
2023-08-10 17:17:28 +0500

    added anonymous function

--- lib/main.dart
+++ lib/main.dart
@@ -25,8 +25,11 @@ class _MyAppState extends State<MyApp> {
       }
     ];
   var _questionIndex = 0; //var is a generic type and dart can infer the type of the variable
-  void _answerQuestion() {
+  var _totalScore = 0;
+  void _answerQuestion(int score) {
+    _totalScore = _totalScore + score;
     setState(() {
+      
       _questionIndex = _questionIndex + 1;
     });
     print(_questionIndex);
