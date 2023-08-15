59b603f78b529aec339fa5f3dcfc0e8c5a29bc27
Farhad Khan Haadi <89342676+FARHADGULL@users.noreply.github.com>
2023-08-15 10:19:40 +0500

    Update answer.dart

--- lib/answer.dart
+++ lib/answer.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 class Answer extends StatelessWidget {
   final VoidCallback selectHandler;
-  
   final String answerText;
   const Answer(this.selectHandler, this.answerText, {super.key});
 
