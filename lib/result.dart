e1ef256bba76c8f94977fd7ba24dcc3f337b79bc
Farhad Khan Haadi <89342676+FARHADGULL@users.noreply.github.com>
2023-08-25 16:44:24 +0500

    Update result.dart

--- lib/result.dart
+++ lib/result.dart
@@ -10,7 +10,7 @@ class Result extends StatelessWidget {
     String resultText;
     if (resultScore >= 30) {
       resultText = 'You are awesome and innocent!';
-    } else if (resultScore > 20 && resultScore <= 30) {
+    } else if (resultScore > 20 && resultScore < 30) {
       resultText = 'Pretty likeable!';
     } else if (resultScore > 10 && resultScore <= 20) {
       resultText = 'You are ... strange?!';
